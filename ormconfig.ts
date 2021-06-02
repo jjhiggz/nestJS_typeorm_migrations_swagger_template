7313b5cf70ca5d0104f1b7a3fafada67e1bfa65e
jjhiggz <jonathan.higger@gmail.com>
2021-06-01 22:37:07 -0600

    test

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,18 +1,15 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-import path from 'path';
-
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const config: PostgresConnectionOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: +process.env.POSTGRES_PORT,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  entities: [path.join(__dirname, '**', '*entity{.ts,.js}')],
-  migrations: ['dist/src/db/migrations/*js'],
+  port: 5432,
+  database: 'nestjs',
+  username: 'admin',
+  password: 'admin',
+  entities: ['dist/src/**/*.entity{.ts,.js}'],
+  migrations: ['dist/src/db/migrations/*{.ts,.js}'],
   cli: {
     migrationsDir: 'src/db/migrations',
   },
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,12 @@
-import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from '../ormconfig';
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
-        PORT: Joi.number(),
-        JWT_SECRET: Joi.string().required(),
-        JWT_EXPIRATION_TIME: Joi.string().required(),
-      }),
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(config), UsersModule],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
